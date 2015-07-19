a6885e0d58224a2cd8f85028de61f9e241618dc6
Jan Strauß <jan@over9000.eu>
2015-07-19 21:09:56 +0200

    qqqq

--- src/main/resources/shaders/fragment.glsl
+++ src/main/resources/shaders/fragment.glsl
@@ -39,7 +39,7 @@ void main() {
 	vec3 N = normal;
 	vec3 L = normalize(lightPosition - position);
 
-	vec4 texColor = texture(textureData, vec3(passTexturePosition.xy, int(passTexturePosition.z)));
+	vec4 texColor = texture(textureData, vec3(passTexturePosition.xy, floor(passTexturePosition.z)));
 
 	vec3 baseColor = vec3(texColor.rgb);
 	if(colorSwitch){
